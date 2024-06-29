9031d7bfda26e0154b18996536673fca90f03171
MOHSEN <mohsen.mhm23@gmail.com>
2024-06-29 11:00:23 +0330

    Minor bugs fixed

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,5 +1,5 @@
 <div id="{{ $name }}" tabindex="-1" aria-hidden="true" wire:ignore.self dir="rtl"
-     class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+     class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full pt-[6rem]">
     <div class="relative p-4 w-full max-w-lg max-h-full">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow overflow-y-auto w-max min-w-96">
